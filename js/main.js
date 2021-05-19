b02cf1e8bcb925ed0b9ca0e11c06b0b7c9bcfcf1
lingchuanfeng <mr.huzx@outlook.com>
2021-05-19 17:27:49 +0800

    Site updated: 2021-05-19 17:27:49

---
+++ js/main.js
@@ -0,0 +1,5 @@
+https://cdn.jsdelivr.net/gh/bynotes/texiao/source/js/jingtaisidai.js # 静态彩带
+https://cdn.jsdelivr.net/gh/bynotes/texiao/source/js/yinghua.js # 樱花飘落
+https://cdn.jsdelivr.net/gh/bynotes/texiao/source/js/timeDate.js # 运行时间
+https://cdn.jsdelivr.net/gh/bynotes/texiao/source/js/love.js # 鼠标点击出小爱心❤
+https://cdn.jsdelivr.net/gh/bynotes/texiao/source/js/xiaoxingxing.js # 鼠标跟随小星星
